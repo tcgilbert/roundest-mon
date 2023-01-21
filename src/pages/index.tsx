3f8887f7b920e69b74eb41a02b54875e7ae2274a
Thomas Gilbert <tcgilbertt@gmail.com>
2023-01-21 13:46:58 -0500

    Add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import { trpc } from '../utils/trpc';
+
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: 'booyah' });
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
@@ -8,6 +12,7 @@ export default function Home() {
         <div className="p-8">Vs.</div>
         <div className="w-16 h-16 bg-red-200">Poke #1</div>
       </div>
+      <div>{hello.data?.greeting}</div>
     </div>
   );
 }
