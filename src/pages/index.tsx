e3d6fd09d25952a757ce84b3047a9b9011435cca
Thomas Gilbert <tcgilbertt@gmail.com>
2023-01-21 13:19:35 -0500

    Basic UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-white">Hello world!</h1>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+      <div className="p-2" />
+      <div className="text-2xl text-center border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200">Poke #1</div>
+        <div className="p-8">Vs.</div>
+        <div className="w-16 h-16 bg-red-200">Poke #1</div>
+      </div>
+    </div>
+  );
 }
