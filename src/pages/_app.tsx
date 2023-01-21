5b37d729cdd5b2194489d1e9722193ac1e12421a
Thomas Gilbert <tcgilbertt@gmail.com>
2023-01-21 17:21:34 -0500

    fixed ssr double renders

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css';
-import type { AppProps, AppType } from 'next/app';
+import type { AppType } from 'next/app';
 import { trpc } from '../utils/trpc';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,34 @@ import { useEffect, useState } from 'react';
 import { trpc } from '../utils/trpc';
 
 export default function Home() {
-  const [first, setFirst] = useState<number>();
-  const [second, setSecond] = useState<number>();
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+  const [first, second] = ids;
 
-  useEffect(() => {
-    const [one, two] = getOptionsForVote();
-    setFirst(one);
-    setSecond(two);
-  }, []);
+  const firstPokemon = trpc.getPokemonById.useQuery({ id: first });
+  const secondPokemon = trpc.getPokemonById.useQuery({ id: second });
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="text-2xl text-center border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img className="w-full" src={firstPokemon.data?.sprite!} />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">Vs.</div>
-        <div className="w-16 h-16 bg-red-800">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img className="w-full" src={secondPokemon.data?.sprite!} />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
